61bdab78c1990e98009b361fcf27bdafb2d8ef31
takujoye14 <takuma.joye414@gmail.com>
2025-04-27 14:09:10 +0200

    fixed routes for porducts

--- frontend-vue/src/pages/EditProduct.vue
+++ frontend-vue/src/pages/EditProduct.vue
@@ -69,7 +69,7 @@ const handleFileChange = (e) => {
 onMounted(async () => {
   try {
     const token = localStorage.getItem('token')
-    const res = await fetch(`http://localhost:3000/api/products/${productId}`, {
+    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/products/${productId}`, {
       headers: { Authorization: `Bearer ${token}` }
     })
     const data = await res.json()
