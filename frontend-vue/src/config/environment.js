de93c29e21b8b2dd605b2a1ff3f26de68d854c66
takujoye14 <takuma.joye414@gmail.com>
2025-04-27 14:22:11 +0200

    environment.js changes

--- frontend-vue/src/config/environment.js
+++ frontend-vue/src/config/environment.js
@@ -1,30 +1,20 @@
-const checkProcess = () => {
-    if (typeof process !== "undefined") {
-     return process.env.NODE_ENV === "production"
-    }
-    return "development"
-   }
-   
-   const getProcess = () => {
-    if (typeof process !== "undefined") {
-     return process.env.API_URL
-    }
-    return "http://localhost"
-   }
-   const getProcessPort = () => {
-    if (typeof process !== "undefined") {
-     return process.env.API_PORT
-    }
-    return 3000
-   }
-   
-   console.log(checkProcess())
-   
-   const GLOBAL_CONFIG = {
-    API_URL: "http://localhost",
-    API_PORT: 3000,
-    APP_ENV: "development"
-  };
-  
-  export default GLOBAL_CONFIG;
-  
+
+const checkAppEnv = () => {
+  return import.meta.env.MODE || "development";
+};
+
+const getApiUrl = () => {
+  return import.meta.env.VITE_API_BASE_URL || "http://localhost";
+};
+
+const getApiPort = () => {
+  return import.meta.env.VITE_API_PORT || 3000;
+};
+
+const GLOBAL_CONFIG = {
+  API_URL: getApiUrl(),
+  API_PORT: getApiPort(),
+  APP_ENV: checkAppEnv(),
+};
+
+export default GLOBAL_CONFIG;
